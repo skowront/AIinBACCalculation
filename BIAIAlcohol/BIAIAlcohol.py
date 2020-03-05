6ad895ebdc0d174b72a311b1fa01ea482db4e08f
V4nish <tomekedison@gmail.com>
2020-03-05 16:46:54 +0100

    classes almost purrfect, it is time to start introducing AI&ML

--- BIAIAlcohol/BIAIAlcohol.py
+++ BIAIAlcohol/BIAIAlcohol.py
@@ -1,8 +1,73 @@
-#imports
+#Application generates data for AI and teaches it to approximate Blood Alcohol Content based on amount of beverages drunk and person parameters.
 
 
 #globals
 separator=","
+configurationFileSeparator=":"
+configurationFile="config.ini"
+
+#configuration
+class Configuration:
+    generateData=0
+    datasetSize=10000
+    roundPlaces=2
+    randomDrinkingTimeMaxHours=24
+    Beverage1MaxAmountML=5000.0
+    Beverage1MinAmountML=0.0
+    Beverage2MaxAmountML=5000.0
+    Beverage2MinAmountML=0.0
+    Beverage3MaxAmountML=5000.0
+    Beverage3MinAmountML=0.0
+    Beverage1PercentageMin=0.0
+    Beverage1PercentageMax=1.0
+    Beverage2PercentageMin=0.0
+    Beverage2PercentageMax=1.0
+    Beverage3PercentageMin=0.0
+    Beverage3PercentageMax=1.0
+    AgeMin=18.0
+    AgeMax=110.0
+    HeightMin=100.0
+    HeightMax=230.0
+    WeightMin=40.0
+    WeightMax=300.0
+    genderDiversity="both"
+    datasetLocation="WidmarkBACdataset.txt"
+    def __init__(self):
+        file=open(configurationFile, "r")
+        self.generateData=int(file.readline().split(configurationFileSeparator)[1])
+        self.roundPlaces=int(file.readline().split(configurationFileSeparator)[1])
+        self.datasetSize=int(file.readline().split(configurationFileSeparator)[1])
+        self.randomDrinkingTimeMaxHours=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage1MaxAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage1MinAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage2MaxAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage2MinAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage3MaxAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage3MinAmountML=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage1PercentageMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage1PercentageMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage2PercentageMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage2PercentageMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage3PercentageMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.Beverage3PercentageMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.AgeMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.AgeMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.HeightMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.HeightMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.WeightMin=float(file.readline().split(configurationFileSeparator)[1])
+        self.WeightMax=float(file.readline().split(configurationFileSeparator)[1])
+        self.GenderDiversity=(file.readline().split(configurationFileSeparator)[1])
+        self.datasetLocation=(file.readline().split(configurationFileSeparator)[1])
+        file.close()
+        return None
+
+#globalConfiguration
+applicationConfiguration=Configuration()
+
+#imports
+import random
+import math
+
 
 #CLASSES needed to store data
 
@@ -24,9 +89,17 @@ class AmountOfAlcohol:
         self.beverage3Percentage=beverage3Percentage
         return None
 
-
     def pureAlcohol(self):
         return beverage1Amount*beverage1Percentage+beverage2Amount*beverage2Percentage+beverage3Amount+beverage3Percentage
+
+    def randomize(self):
+        self.beverage1Amount=round(random.uniform(applicationConfiguration.Beverage1MinAmountML,applicationConfiguration.Beverage1MaxAmountML),applicationConfiguration.roundPlaces)
+        self.beverage2Amount=round(random.uniform(applicationConfiguration.Beverage2MinAmountML,applicationConfiguration.Beverage2MaxAmountML),applicationConfiguration.roundPlaces)
+        self.beverage3Amount=round(random.uniform(applicationConfiguration.Beverage3MinAmountML,applicationConfiguration.Beverage3MaxAmountML),applicationConfiguration.roundPlaces)
+        self.beverage1Percentage=round(random.uniform(applicationConfiguration.Beverage1PercentageMin,applicationConfiguration.Beverage1PercentageMax),applicationConfiguration.roundPlaces)
+        self.beverage2Percentage=round(random.uniform(applicationConfiguration.Beverage2PercentageMin,applicationConfiguration.Beverage2PercentageMax),applicationConfiguration.roundPlaces)
+        self.beverage3Percentage=round(random.uniform(applicationConfiguration.Beverage3PercentageMin,applicationConfiguration.Beverage3PercentageMax),applicationConfiguration.roundPlaces)
+        return self
 
     def amountOfAlcoholToString(self):
         return str(self.beverage1Amount).ljust(len("Bev1Amount "))+separator+str(self.beverage1Percentage).ljust(len("Bev1% "))+separator+str(self.beverage2Amount).ljust(len("Bev2Amount "))+separator+str(self.beverage2Percentage).ljust(len("Bev2% "))+separator+str(self.beverage3Amount).ljust(len("Bev3Amount "))+separator+str(self.beverage3Percentage).ljust(len("Bev3% "))
@@ -55,7 +128,6 @@ class Person:
         return None
 
 
-
     #Watson formula for body liquid calculation 
     #MALE
     #tbw = 2.447 - (0.09156*L) + (0.1074*W) + (0.3362*M)
@@ -67,6 +139,18 @@ class Person:
             return 2.447 - (0.09156*age) + (0.1074*height) + (0.3362*weight)
         else:
             return -2.097 + (0.1069*height) +(0.2466*weight)
+
+    def randomize(self):
+        self.age=round(random.uniform(applicationConfiguration.AgeMin,applicationConfiguration.AgeMax),applicationConfiguration.roundPlaces)
+        self.height=round(random.uniform(applicationConfiguration.HeightMin,applicationConfiguration.HeightMax),applicationConfiguration.roundPlaces)
+        self.weight=round(random.uniform(applicationConfiguration.WeightMin,applicationConfiguration.WeightMax),applicationConfiguration.roundPlaces)
+        if applicationConfiguration.genderDiversity=="both":
+            self.gender=float(random.randint(0,1))
+        if applicationConfiguration.genderDiversity=="male":
+            self.gender=0.0
+        if applicationConfiguration.genderDiversity=="female":
+            self.gender=1.0
+        return self
 
     def personToString(self):
         return " "+str(self.age).ljust(len("Age   "))+separator+str(self.height).ljust(len("Height "))+separator+str(self.weight).ljust(len("Weight "))+separator+str(self.gender).ljust(len("Gender "))
@@ -113,6 +197,13 @@ class BloodAlcoholContent:
             decay=0.2
         return (amountOfAlcohol.pureAlcohol()/person.calculateBodyLiquids())*alcoholDensity-(drinkingTime*decay);
 
+    def randomize(self):
+        self.person=Person().randomize()
+        self.amountOfAlcohol=AmountOfAlcohol().randomize()
+        self.drinkingTime=round(random.uniform(0.0,applicationConfiguration.randomDrinkingTimeMaxHours),applicationConfiguration.roundPlaces)
+        self.drinksOften=random.randint(0,2)
+        return self
+
     def bloodAlcoholContentToString(self):
         tempPerson=self.person.personToString()
         tempAoA=self.amountOfAlcohol.amountOfAlcoholToString()
@@ -127,25 +218,20 @@ class BloodAlcoholContent:
         self.drinksOften=float(input.split(separator)[11].replace(" ",""))
         return None
 
-BAC=BloodAlcoholContent(Person(20.0,180.0,65.0,0.0),AmountOfAlcohol(1000.0,0.5,500.0,0.12,0.0,0.30),0,1.0)
+#APPLICATION START
 
-fileHeader="|Age   |Height |Weight |Gender |Bev1Amount |Bev1% |Bev2Amount |Bev2% |Bev3Amount |Bev3% |DrinkingTime |DrinksOften |"
-
-
-
-file= open("WidmarkBACdataset.txt", "r")
-s1=file.readline()
-s2=file.readline()
-print(s1)
-BAC=BloodAlcoholContent()
-BAC.bloodAlcoholContentFromString(s2)
-print(BAC.bloodAlcoholContentToString())
-file.write(BAC.bloodAlcoholContentToString())
-file.close()
+#check if user wants to generate dataset
+if applicationConfiguration.generateData==1:
+    #generate random data and send it to file
+    fileHeader="|Age   |Height |Weight |Gender |Bev1Amount |Bev1% |Bev2Amount |Bev2% |Bev3Amount |Bev3% |DrinkingTime |DrinksOften |"
+    file=open(applicationConfiguration.datasetLocation, "w")
+    _=file.write(fileHeader+"\n")
+    for i in range(0,applicationConfiguration.datasetSize):
+        _=file.write(BloodAlcoholContent().randomize().bloodAlcoholContentToString()+"\n")
+    file.close()
 
 
-file= open("WidmarkBACdataset.txt", "w")
-file.write(fileHeader+"\n")
-for i in range(0,6):
-    file.write(BAC.bloodAlcoholContentToString()+"\n")
-file.close()
+
+
+
+
